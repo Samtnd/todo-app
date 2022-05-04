91a174e7fc7a1ef610fad43cdde532634591bc40
tundae_s <tundae_s@etna-alternance.net>
2022-05-04 09:37:54 +0200

    TodoList

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import React from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const[todos, setTodos] = React.useState([]) 
+  const [todo, setTodo] = React.useState("")
+
+  React.useEffect(() => { //Only run the effect on the initial render
+    const temp = localStorage.getItem("todos") //getting localstorage
+    const loadedTodos = JSON.parse(temp) //transforming json string into a js object
+
+    if (loadedTodos) {
+      setTodos(loadedTodos)
+    }
+  }, [])
+
+  React.useEffect(() => {
+    const tmp = JSON.stringify(todos)
+    localStorage.setItem("todos", tmp)
+    
+  }, [todos])
+
+  function handleSubmit(e){
+    //otherwise the page will reload
+    e.preventDefault()
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo, 
+      completed: false,
+    }
+
+    setTodos([...todos].concat(newTodo))
+    setTodo("")
+  }
+
+  function deleteTodo(id){
+    const updateTodos = [...todos].filter((todo) => todo.id !== id ) //copy the table
+
+    setTodos(updateTodos)
+  }
+
+  function toggleComplete(id){
+    const updateTodos = [...todos].map((todo) => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+
+    setTodos(updateTodos)
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1 align="center">My Todo App</h1>
+      <form className="todo-form" onSubmit={handleSubmit}>
+        <input type="text" placeholder="Fill in the task" onChange={(e) => setTodo(e.target.value)} value={todo}/>
+        <button type="submit"><i className="fas fa-plus"></i></button>
+      </form>
+      {todos.map((todo) => <div className="todo-list" key={todo.id}>
+        <div>{todo.text}</div>
+        <button class="icon" onClick={() => deleteTodo(todo.id)}><i className="fas fa-trash"></i></button>
+        <div className="form-check form-switch">
+          <input
+            className="form-check-input" 
+            type="checkbox" 
+            onChange={() => toggleComplete(todo.id)} 
+            checked={ todo.completed}/>
+        </div>
+      </div>)}
     </div>
   );
 }
